45b4d819b9e8b44c5b4975798a3cc808a71b8932
pandeyaman <pandey.aman111@gmail.com>
2020-12-06 22:47:26 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+var inputText = document.querySelector('#input-text');
+var btnTransalate = document.querySelector('#btn-translate');
+var outputText = document.querySelector('#output-text');
+var sampleText = document.querySelector("#sample-text");
+var resettext = document.querySelector("#reset-text");
+
+var funFactSpan = document.querySelector('#fact-span');
+
+var facts = [
+    "Minions are either one-eyed or two-eyed and all of them wear metallic goggles accommodating the number of eyes.",
+    "All minions wear goggles without any specific reasons. And they have been wearing them since their existence in prehistoric era.",
+    "Minions without goggles look horrible. Trust me.",
+    "Minions have only 3 fingers in each hand.",
+    "They have limited hairstyles; tall & short buzz cut, combed center parted, sprout cut, spread out spiky and bald.",
+    "Minionese is more than words, like any other languages, it has tone of voice, body language & expressions. Minionese is more pragmatic than semantic.",
+    "Though minions were first introduced to us in 2010, they have been living on this earth since forever. They are an ancient and pre-historic race.",
+    "Minions have Standard English names; Kevin, Stuart, Dave, Carl, Bob, Donny, Tom, Phil, Steve, Chris, John and many more.",
+    "Stuart is the only minion to be identified in all 3 movies; Despicable Me, Despicable Me 2 and Minions retaining the same appearance under his name, Stuart.",
+    "Purple and yellow are on opposite sides in color spectrum; hence the evil minions were made purple.",
+];
+
+function randomFact(){
+    var index = Math.floor(Math.random()*10);
+    var fact = facts[index];
+    return fact;
+}
+
+
+btnTransalate.addEventListener('click', () => {
+    var englishText = inputText.value;
+    var URL = 'https://api.funtranslations.com/translate/mandalorian.json?text=' + englishText;
+    fetch(URL)
+        .then(response => response.json())
+        .then(json => outputText.innerText = json.contents.translated)
+        .catch(() => {
+            alert("This API is rate-limited and can be called only 5 times per hour. Please try again after an hour.");
+        })
+
+    var funFact = randomFact();
+    funFactSpan.textContent =funFact;
+});
+
+sampleText.addEventListener('click', () => {
+    inputText.innerText = "";
+    inputText.value = 'Hello ! I am a minion and I love Banana !';
+});
+
+resettext.addEventListener('click', () => {
+    inputText.value = " ";
+    outputText.innerText = " ";
+});
